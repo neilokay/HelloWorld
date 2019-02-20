d9cec7896006b1fea7eb4c7fd5bb7557b0fd3067
neilokay <root@localhost>
2019-02-20 17:37:03 +0800

    modified Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
-public calss Hello{
+public class Hello{
 	
 	public static void main(String[] args){
 		
-		for (int i=1;i<=10;i++){
+		for (int i = 1;i <= 10; i++){
 			System.out.println("Hello java ... " + i);	
 		}
 	}
-}
+
